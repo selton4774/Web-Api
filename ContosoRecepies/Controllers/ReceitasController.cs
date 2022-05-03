2f60ffbd584f8171ad82c75aa0b7ca98b77b9fdc
Selton Farias <seltonfarias4@icloud.com>
2022-05-03 10:08:38 -0300

    [4/18] - cria uma Controller

---
+++ ContosoRecepies/Controllers/ReceitasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecepies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceitasController : ControllerBase
+    {
+        [HttpGet]
+        public String[] ObterPratos()
+        {
+            String[] pratos = { "Arroz com feijão", "Galinhada", "Sopa de banana" };
+            return pratos;
+        }
+    }
+}
