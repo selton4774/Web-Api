329a431d5ed2a7568d28e475013da4a4ba28f8ba
Selton Farias <seltonfarias4@icloud.com>
2022-05-05 08:48:36 -0300

    [7/18] - Swagger UI

--- ContosoRecepies/Controllers/ReceitasController.cs
+++ ContosoRecepies/Controllers/ReceitasController.cs
@@ -7,15 +7,51 @@ using System.Threading.Tasks;
 
 namespace ContosoRecepies.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/receitas")]
     [ApiController]
     public class ReceitasController : ControllerBase
     {
+        String[] receitas = { "Arroz com feijão", "Galinhada", "Sopa de banana" };
+
         [HttpGet]
-        public String[] ObterPratos()
+        public IActionResult ObterTodasAsReceitas()
         {
-            String[] pratos = { "Arroz com feijão", "Galinhada", "Sopa de banana" };
-            return pratos;
+
+
+            if (receitas.Length < 1) // receitas.Any(), não funcionou
+            {
+                return NotFound();
+            }
+
+            return Ok(receitas);
+        }
+
+        [HttpPut("{nomeAntigo}, {NomeNovo}")]
+        public IActionResult AtualizarReceita(string nomeAntigo, string novoNome)
+        {
+            for (int i = 0; i < receitas.Length - 1; i++)
+            {
+                if(receitas[i] == nomeAntigo)
+                {
+                    receitas[i] = novoNome;
+                    return NoContent();
+                }
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarReceita(string id)
+        {
+            bool AlgoDeuErrado = false;
+
+            if(AlgoDeuErrado)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
         }
     }
 }
